e6df104196a7f8960bf7b47e1c4148655bed015a
Nick Lim <nicklimvs@gmail.com>
2024-10-26 12:48:01 +0800

    Update index.ts

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,3 +1,4 @@
 export { default as NixButton } from './NixButton.vue';
 export { default as NixDropdownButton } from './NixDropdownButton.vue';
 export { default as NixRadioButton } from './NixRadioButton.vue';
+export { default as NixButtonGroup } from './NixButtonGroup.vue';
