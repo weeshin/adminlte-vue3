060e222220f926308a830868c0efa00e7a486b83
Nick Lim <nicklimvs@gmail.com>
2024-11-19 00:06:34 +0800

    create scss

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,13 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-      
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true
+      }
+    }
+  },
   plugins: [
     vue(),
     dts({
